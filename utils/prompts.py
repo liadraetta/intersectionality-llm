bbd4fc94a2a114c5a71d02ad031b62c6c7f06bfc
SodaMaremLo <soda.marem.lo@gmail.com>
2025-06-04 10:27:30 +0200

    adj prompts

--- utils/prompts.py
+++ utils/prompts.py
@@ -1,4 +1,9 @@
-def prompt_1(text, demographics):
-    prompt = ".........."
+def prompt_baseline_1(text):
+    prompt = "......." \
+    f"text: {text}"
+    "could the post be offensive to anyone"
+    "was the intent behind the statement to offend"
+    "provide a label 0,1"
+    "provide a brief explanation of your choice"
 
     return prompt 
