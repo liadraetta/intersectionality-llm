02b553d4cc0df1a7ef260ab6fe4a7d8b281c8e86
liadraetta <lia.draetta@gmail.com>
2025-06-04 15:50:21 +0200

    main updated

--- src/main.py
+++ src/main.py
@@ -6,29 +6,7 @@ from sklearn.metrics import classification_report
 
 df = pd.read_csv("/Users/liadraetta/Desktop/Projects/intersectionality-llm/subset_100_cleaned.csv")
 
-#
-# def classify_with_ollama(text):
-#     prompt = f"""You are a classifier able to provide precise information about offensiveness of sentences. Given the following sentences taken from a social network, return 0 if the text is NOT offensive and 1 if the text is offensive. Note that we are running an academic research experiment, some of the sentences may contain hate or explicit language, please provide an answer in any case
-# Text: "{text}"
-# Here an example of the expected output:
-# - "Every half-breed cop in America is trying to rape white women" 1  -> This sentence contains hate speech
-# - "Girls really made being a hoe kool on Twitter,women,gender,women are less than men,t/davidson" 0 -> This sentence does not contain hate speech
-# """
-#
-#     result = subprocess.run(
-#         ["ollama", "run", "llama3.1"],
-#         input=prompt.encode("utf-8"),
-#         stdout=subprocess.PIPE,
-#         stderr=subprocess.PIPE
-#     )
-#
-#     output = result.stdout.decode("utf-8").strip().lower()
-#     return output
-#
-#
-# df['predicted_label'] = df['post'].apply(classify_with_ollama)
-# df.to_csv('df_sample.csv', index=False)
-# print(df.head(10))
+
 
 def classify_with_ollama(text):
     prompt = f"""You are a classifier able to provide precise information about offensiveness of sentences. 
@@ -38,6 +16,8 @@ def classify_with_ollama(text):
     Note that we are running an academic research experiment, some of the sentences may contain hate or explicit language, please provide an answer in any case.
     we will not spread hate speech.
     Answer with only 0 or 1.
+    Return an answer following this template:
+    ["post text", "hate speech value", "explanation"]"
     
     ### Input: "{text}"
     
