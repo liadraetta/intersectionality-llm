f2543f3353cf990beeb748da1e8b4c30f8171969
SodaMaremLo <soda.marem.lo@gmail.com>
2025-07-21 16:23:46 +0200

    plot offensiveness

--- prepare_data.py
+++ prepare_data.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 from utils.dataset_info import *
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 df = pd.read_csv("data/annWithAttitudes/largeScale.csv")
 """original columns
@@ -27,7 +29,7 @@ df = df[[
 df_demographics = df[["annId", "annotatorGender", "annotatorPolitics", "annotatorRace", "annotatorAge", "annotatorMinority"]]
 df_demographics = df_demographics.drop_duplicates(subset="annId")
 
-df_demographics.to_csv("./dataset/AnnAttDemographics.csv", index=False)
+# df_demographics.to_csv("./dataset/AnnAttDemographics.csv", index=False)
 
 
 
@@ -35,19 +37,56 @@ df_demographics.to_csv("./dataset/AnnAttDemographics.csv", index=False)
 df["offensiveYN"] = df.apply(binary_offensiveness, axis=1)
 df["annotatorPoliticsBinary"] = df.apply(binary_politics, axis=1)
 
+
 # Remove unwanted annotators and neutral labels 
 df_cleaned = clean_dataset(df)
 
-print("original dataframe: ", df.shape)
-print("cleaned dataframe: ", df_cleaned.shape)
-print()
-
-# Dataset information and statistics
-print_info(df_cleaned)
 
 
-df_cleaned.to_csv("./dataset/AnnAttDataset.csv", index=False)
+# df_cleaned.to_csv("./dataset/AnnAttDataset.csv", index=False)
 
 
 # subset_100 = df_cleaned.sample(100, random_state=42)
 # subset_100.to_csv("./dataset/subset_100.csv", index=False)
+
+
+
+# Print statistics
+print("original dataframe: ", df.shape)
+print("cleaned dataframe: ", df_cleaned.shape)
+print("-"*100)
+
+print("original dataframe statistics: ")
+print("*"*25)
+print_info(df)
+
+print("cleaned dataframe statistics: ")
+print("*"*25)
+print_info(df_cleaned)
+
+print("-"*100)
+
+
+
+
+# Prepare data
+counts = df["off_avg"].value_counts().sort_index()
+plot_data = counts.reset_index()
+plot_data.columns = ["off_avg", "count"]
+
+# Plot using Seaborn
+plt.figure(figsize=(8, 5))
+sns.barplot(data=plot_data, x="off_avg", y="count", palette="Blues")
+
+# Add labels and title
+plt.xlabel("Offensive Average")
+plt.ylabel("Frequency")
+
+# Add values on top
+for i in range(len(plot_data)):
+    plt.text(i, plot_data["count"][i] + 10, plot_data["count"][i], ha='center', fontsize=9)
+
+plt.tight_layout()
+plt.savefig("./dataset/plot_offensiveness.png", dpi=300)
+plt.close()
+
